43750d8903e2d8e0cb0e2df628f6749bd5bfff01
Sérgio Freitas <sjcsgfreitas@hotmail.com>
2026-02-02 22:03:51 +0000

    Add render hosts to allowedHosts.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import dns from 'node:dns'
+import { defineConfig } from "vite";
+import dns from "node:dns";
 
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
   server: {
-    allowedHosts: ['www.palais-freitas.xyz', 'palais-freitas.xyz']
+    allowedHosts: [
+      "www.palais-freitas.xyz",
+      "palais-freitas.xyz",
+      "website-4gnq.onrender.com",
+      "www.website-4gnq.onrender.com",
+    ],
   },
-})
+});
